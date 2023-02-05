d9f5a2dfbc4d3343f0b666cf4192ef55a575391c
IQuant <quant3234@gmail.com>
2023-02-05 20:26:41 +0300

    Added gameplay

--- src/lib.rs
+++ src/lib.rs
@@ -1,26 +1,81 @@
-//#![allow(dead_code)]
+use std::collections::HashMap;
 
-use manual::ManualBoard;
+use crate::manual::Manual;
+use crate::manual::ManualBoard;
 use notan::draw::*;
-use notan::log;
 use notan::prelude::*;
-//use notan::text::*;
+use notan::random::rand::seq::SliceRandom;
+use notan::random::rand::thread_rng;
+use serde::Deserialize;
 
 mod manual;
+
+/*
+clear: clear terminal
+cat: print content of file
+ls: see files/folder in current folder
+ssh [ip]: connect to a host
+dc: disconnect from host
+print: print a manual file
+cd: navigate files (low priority because needs filesystem to work)
+
+hack [ip]: start puzzle associated to host
+*/
+
+#[derive(Deserialize)]
+enum File {
+    Manual { id: String },
+}
+
+#[derive(Deserialize)]
+struct Host {
+    fs: HashMap<String, File>,
+    hacked: bool,
+    position: (f32, f32),
+    puzzle_kind: String,
+}
+
+#[derive(Deserialize, Default)]
+struct Network {
+    hosts: HashMap<String, Host>,
+}
+
+#[derive(Default)]
+enum ConsoleState {
+    #[default]
+    AwaitingCommand,
+    AwaitingAnswer {
+        answer: String,
+        host: String,
+    },
+}
 
 #[derive(Default)]
 struct Console {
     lines: Vec<String>,
     prompt: String,
+    state: ConsoleState,
+    current_host: String,
 }
 
 impl Console {
     pub fn get_full_prompt(&self) -> String {
-        format!("root@192.168.0.7> {}", self.prompt)
+        match &self.state {
+            ConsoleState::AwaitingCommand => format!("root@{}> {}", self.current_host, self.prompt),
+            ConsoleState::AwaitingAnswer { answer: _, host } => {
+                format!("hacking {}> {}", host, self.prompt)
+            }
+        }
     }
     pub fn add_line(&mut self, line: String) {
         self.lines.push(line);
     }
+}
+
+#[derive(Deserialize)]
+struct Puzzle {
+    //manual_name: String,
+    answers: Vec<(String, String)>,
 }
 
 #[derive(AppState)]
@@ -30,6 +85,89 @@ pub struct State {
     time: f32,
     manual_board: ManualBoard,
     previous_mouse_pos: (i32, i32),
+    network: Network,
+    manuals: HashMap<String, Manual>,
+    puzzles: HashMap<String, Puzzle>,
+}
+
+impl State {
+    fn handle_command(&mut self) {
+        let prompt = &self.console.prompt;
+        let mut args = prompt.split_whitespace();
+        let Some(cmd) = args.next() else {return};
+        match cmd {
+            "clear" => self.console.lines.clear(),
+            "ls" => {
+                for key in self.network.hosts[&self.console.current_host].fs.keys() {
+                    self.console.add_line(key.clone());
+                }
+            }
+            "print" => {
+                let Some(arg) = args.next() else {self.console.add_line("Filename expected".to_string());return};
+                let Some(File::Manual { id }) = self.network.hosts[&self.console.current_host].fs.get(arg) else {self.console.add_line("Not a manual page".to_string());return};
+                // TODO: actually print the manual
+                self.manual_board.print_manual(self.manuals[id].clone());
+            }
+            "ssh" => {
+                if let Some(arg) = args.next() {
+                    if self.network.hosts.contains_key(arg) {
+                        if self.network.hosts[arg].hacked {
+                            self.console.current_host = arg.to_owned();
+                        } else {
+                            self.console
+                                .add_line(format!("Connection refused: access denied"));
+                        }
+                    } else {
+                        self.console
+                            .add_line(format!("Connection refused: no host found"));
+                    }
+                } else {
+                    self.console.add_line(format!("Expected address"));
+                }
+            }
+            "hack" => {
+                if let Some(arg) = args.next() {
+                    if self.network.hosts.contains_key(arg) {
+                        let host = &self.network.hosts[arg];
+                        if !host.hacked {
+                            //TODO puzzle
+                            let puzzle = &self.puzzles[&host.puzzle_kind];
+                            let (query, answer) = puzzle.answers.choose(&mut thread_rng()).unwrap();
+                            self.console.state = ConsoleState::AwaitingAnswer {
+                                answer: answer.to_string(),
+                                host: arg.to_owned(),
+                            };
+                            self.console.add_line(query.to_owned());
+                            //self.network.hosts.get_mut(arg).unwrap().hacked = true;
+                        } else {
+                            self.console.add_line(format!("Error: already root"));
+                        }
+                    } else {
+                        self.console
+                            .add_line(format!("Connection refused: no host found"));
+                    }
+                } else {
+                    self.console.add_line(format!("Expected address"));
+                }
+            }
+            "dc" => self.console.current_host = "127.0.0.1".to_string(),
+            _ => self.console.add_line(format!("Unknown command: {cmd}")),
+        }
+    }
+
+    fn handle_console_prompt(&mut self) {
+        match &self.console.state {
+            ConsoleState::AwaitingCommand => self.handle_command(),
+            ConsoleState::AwaitingAnswer { answer, host } => {
+                if answer == &self.console.prompt {
+                    self.network.hosts.get_mut(host).unwrap().hacked = true;
+                } else {
+                    self.console.add_line("Hacking attempt failed".to_string());
+                }
+                self.console.state = ConsoleState::AwaitingCommand;
+            }
+        };
+    }
 }
 
 pub fn setup(app: &mut App, gfx: &mut Graphics) -> State {
@@ -40,26 +178,81 @@ pub fn setup(app: &mut App, gfx: &mut Graphics) -> State {
 
     // BUG: set_size not being applied directly when not in html page, making width and height wrong
     // println!("{} {}", app.window().width(), app.window().height());
-    println!("{} {}", gfx.size().0 , gfx.size().1 );
+    // println!("{} {}", gfx.size().0, gfx.size().1);
 
-    State {
+    let mut state = State {
         font,
         time: 0.0,
         console: Default::default(),
         manual_board: ManualBoard::new(app.window().width(), app.window().height()),
         previous_mouse_pos: (0, 0),
-    }
+        network: serde_json::from_str(include_str!("network_desc.json"))
+            .map_err(|err| {
+                notan::log::error!("{err}");
+                err
+            })
+            .unwrap_or_default(),
+        puzzles: serde_json::from_str(include_str!("puzzles.json")).unwrap(),
+        manuals: HashMap::new(),
+    };
+    state.console.current_host = "127.0.0.1".to_string();
+    state.manuals.insert(
+        "commands".to_string(),
+        Manual::new(
+            // setup manuals (move this in its own function or something)
+            (0, 0),
+            (300, 200),
+            include_str!("manuals/commands.txt").to_string(),
+            (209, 190, 161),
+        ),
+    );
+    state.manuals.insert(
+        "hack".to_string(),
+        Manual::new(
+            // setup manuals (move this in its own function or something)
+            (0, 0),
+            (250, 100),
+            include_str!("manuals/hack.txt").to_string(),
+            (220, 252, 199),
+        ),
+    );
+    state.manuals.insert(
+        "puzzle1".to_string(),
+        Manual::new(
+            (0, 0),
+            (500, 250),
+            include_str!("manuals/puzzle1.txt").to_string(),
+            (220, 152, 199),
+        ),
+    );
+
+    state
+        .manual_board
+        .print_manual(state.manuals["commands"].clone());
+    // state
+    //     .manual_board
+    //     .print_manual(state.manuals["hack"].clone());
+    // state
+    //     .manual_board
+    //     .print_manual(state.manuals["puzzle1"].clone());
+
+    state
 }
 
 pub fn event(app: &mut App, state: &mut State, event: Event) {
     match event {
-        Event::ReceivedCharacter(c) if c != '\u{7f}' => {
+        Event::ReceivedCharacter(c) if !c.is_ascii_control() => {
             if state.console.prompt.len() < 70 {
                 state.console.prompt.push(c);
             }
         }
         Event::KeyDown { key } if key == KeyCode::Back => {
             state.console.prompt.pop();
+        }
+        Event::MouseDown { button: _, x, y } => {
+            state
+                .manual_board
+                .mouse_drag((x, y), state.previous_mouse_pos);
         }
         Event::MouseMove { x, y } => {
             if app.mouse.is_down(MouseButton::Left) {
@@ -76,9 +269,14 @@ pub fn event(app: &mut App, state: &mut State, event: Event) {
 pub fn update(app: &mut App, state: &mut State) {
     if app.keyboard.was_pressed(KeyCode::Return) {
         state.console.add_line(state.console.get_full_prompt());
+        state.handle_console_prompt();
         state.console.prompt.clear();
     }
     state.time += app.system_timer.delta_f32();
+
+    state
+        .manual_board
+        .update_printer(app.system_timer.delta_f32());
 }
 
 pub fn draw(gfx: &mut Graphics, state: &mut State) {
@@ -87,22 +285,65 @@ pub fn draw(gfx: &mut Graphics, state: &mut State) {
 
     let (width, height) = (gfx.size().0 as f32, gfx.size().1 as f32);
 
-
-    // manual background
+    // manualboard background
     draw.rect((width / 2.0, height * 0.25), (width / 2.0, height * 0.75))
         .color(Color::GRAY);
 
-    for manual in state.manual_board.manuals.iter() {
+    for manual in state.manual_board.manuals.iter().rev() {
         draw.rect(
             (manual.pos.0 as f32, manual.pos.1 as f32),
             (manual.size.0 as f32, manual.size.1 as f32),
         )
-        .color(Color::RED);
+        .color(Color {
+            r: manual.bg_color.0 as f32 / 255.0,
+            g: manual.bg_color.1 as f32 / 255.0,
+            b: manual.bg_color.2 as f32 / 255.0,
+            a: 1.0,
+        });
+
+        draw.text(&state.font, &manual.text)
+            .color(Color::BLACK)
+            // .position(10.0, 5.0 + i_f * line_height)
+            .position(manual.pos.0 as f32 + 6.0, manual.pos.1 as f32 + 6.0)
+            .size(14.0);
+    }
+
+    // draw printing manual
+    for mp in state.manual_board.manual_printing.iter().rev() {
+        draw.rect(
+            (mp.manual.pos.0 as f32, mp.manual.pos.1 as f32),
+            (mp.manual.size.0 as f32, mp.manual.size.1 as f32),
+        )
+        .color(Color {
+            r: mp.manual.bg_color.0 as f32 / 255.0,
+            g: mp.manual.bg_color.1 as f32 / 255.0,
+            b: mp.manual.bg_color.2 as f32 / 255.0,
+            a: 1.0,
+        });
+
+        draw.text(&state.font, &mp.manual.text)
+            .color(Color::BLACK)
+            // .position(10.0, 5.0 + i_f * line_height)
+            .position(mp.manual.pos.0 as f32 + 6.0, mp.manual.pos.1 as f32 + 6.0)
+            .size(14.0);
     }
 
     // graph background
     draw.rect((width / 2.0, 0.0), (width / 2.0, height * 0.25))
-        .color(Color::WHITE);
+        .color(Color::from_hex(0x303030ff));
+
+    for (addr, host) in state.network.hosts.iter() {
+        let color = if host.hacked {
+            Color::GREEN
+        } else {
+            Color::RED
+        };
+        draw.circle(10.0)
+            .position(width / 2.0 + host.position.0, host.position.1)
+            .color(color);
+        draw.text(&state.font, &addr)
+            .position(width / 2.0 + host.position.0 + 13.0, host.position.1 - 9.0);
+    }
 
     // terminal background
     draw.rect((0.0, 0.0), (width / 2.0, height))
--- src/manual.rs
+++ src/manual.rs
@@ -1,5 +1,3 @@
-use notan::{input::mouse, log::debug};
-
 fn point_aabb(pos: (i32, i32), rect_pos: (i32, i32), rect_size: (i32, i32)) -> bool {
     return rect_pos.0 < pos.0
         && pos.0 < rect_pos.0 + rect_size.0
@@ -7,16 +5,29 @@ fn point_aabb(pos: (i32, i32), rect_pos: (i32, i32), rect_size: (i32, i32)) -> b
         && pos.1 < rect_pos.1 + rect_size.1;
 }
 
-#[derive(Default)]
+// #[derive(Default)]
+// #[derive(Copy, Clone)]
+#[derive(Default, Clone)]
 pub struct Manual {
     pub pos: (i32, i32),
     pub size: (i32, i32),
-    // images
+    pub text: String,
+    pub bg_color: (i32, i32, i32),
 }
 
 impl Manual {
-    pub fn new(center: (i32, i32), size: (i32, i32)) -> Manual {
-        let mut manual = Manual { pos: (0, 0), size };
+    pub fn new(
+        center: (i32, i32),
+        size: (i32, i32),
+        text: String,
+        bg_color: (i32, i32, i32),
+    ) -> Manual {
+        let mut manual = Manual {
+            pos: (0, 0),
+            size,
+            text,
+            bg_color,
+        };
 
         manual.set_center(center);
 
@@ -32,7 +43,17 @@ impl Manual {
 pub struct ManualBoard {
     pub pos: (i32, i32),
     pub size: (i32, i32),
-    pub manuals: [Manual; 1],
+    pub manuals: Vec<Manual>,
+    pub manual_printing: Vec<PrintAnimation>,
+}
+
+// pub struct PrintAnimation<'a> {
+pub struct PrintAnimation {
+    // pub manual: &'a Manual,
+    pub manual: Manual,
+    pub moving_timer: f32, // 400ms
+    pub pause_timer: f32,  // 200ms
+    pub is_moving: bool,   // bool
 }
 
 impl ManualBoard {
@@ -40,37 +61,83 @@ impl ManualBoard {
         let pos: (i32, i32) = (window_width / 2, ((window_height as f32) * 0.25) as i32);
         let size: (i32, i32) = (window_width / 2, ((window_height as f32) * 0.75) as i32);
 
-        println!("{} {}", window_width, window_height);
+        // println!("{} {}", window_width, window_height);
         // println!("{} {}, {} {}", pos.0, pos.1, size.0, size.1);
 
         ManualBoard {
             pos,
             size,
-            manuals: [Manual::new(
-                (pos.0 + size.0 / 2, pos.1 + size.1 / 2),
-                (200, 400),
-            )],
+            manuals: Vec::new(),
+            manual_printing: Vec::new(),
         }
     }
-    pub fn center(&self) -> (i32, i32) {
+
+    fn center(&self) -> (i32, i32) {
         (
             self.pos.0 + (self.size.0 / 2),
             self.pos.1 + (self.size.1 / 2),
         )
     }
 
+    pub fn update_printer(&mut self, dt: f32) {
+        if self.manual_printing.len() <= 0 {
+            return;
+        };
+
+        let pm = &mut self.manual_printing[0];
+
+        let board_y_bottom = self.pos.1 + self.size.1;
+
+        if pm.manual.pos.1 + pm.manual.size.1 < board_y_bottom {
+            self.manuals.insert(0, pm.manual.clone());
+            self.manual_printing.remove(0);
+            return;
+        }
+
+        if pm.is_moving {
+            pm.moving_timer -= dt;
+            pm.manual.pos.1 -= 2;
+            if pm.moving_timer < 0.0 {
+                pm.is_moving = false;
+                pm.moving_timer = 0.3;
+            }
+        } else {
+            pm.pause_timer -= dt;
+            if pm.pause_timer < 0.0 {
+                pm.pause_timer = 0.15;
+                pm.is_moving = true;
+            }
+        }
+    }
+
+    pub fn print_manual(&mut self, mut manual: Manual) {
+        let center = self.center();
+        manual.pos = (center.0 - manual.size.0 / 2, self.pos.1 + self.size.1);
+
+        let pa = PrintAnimation {
+            manual,
+            moving_timer: 0.3,
+            pause_timer: 0.15,
+            is_moving: true,
+        };
+        self.manual_printing.push(pa);
+    }
+
     pub fn mouse_drag(&mut self, mouse_pos: (i32, i32), previous_mouse_pos: (i32, i32)) {
-        if !point_aabb(mouse_pos, self.pos, self.size) { return };
+        if !point_aabb(mouse_pos, self.pos, self.size) {
+            return;
+        };
 
         // println!("{} {}", mouse_pos.0, mouse_pos.1);
-
 
         let mut is_dragged = false;
 
         // move element in front of the list (will be first to rendered)
         for (i, manual) in self.manuals.iter().enumerate() {
             if point_aabb(previous_mouse_pos, manual.pos, manual.size) {
-                self.manuals.swap(0, i);
+                // self.manuals.swap(0, i);
+                let m = self.manuals.remove(i);
+                self.manuals.insert(0, m);
                 is_dragged = true;
                 break;
             }
@@ -82,10 +149,10 @@ impl ManualBoard {
 
             self.manuals[0].pos.0 += dx;
             self.manuals[0].pos.1 += dy;
+            // self.manuals.last_mut().unwrap().pos.0 += dx;
+            // self.manuals.last_mut().unwrap().pos.1 += dy;
 
             // TODO: add check so manual don't go too much outside of it reserved space
         }
     }
 }
-//
-
